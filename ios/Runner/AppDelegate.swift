241745eac06d5529b55c1943c42a07e0e79fb275
Amssel picado Hernandez <amsselherd@gmail.com>
2025-09-08 00:22:16 -0600

    feat(ios): update AppDelegate and Info.plist for Firebase and notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+// GoogleMaps removido: usamos flutter_map con OpenStreetMap
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // No se necesita inicializar Google Maps
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
